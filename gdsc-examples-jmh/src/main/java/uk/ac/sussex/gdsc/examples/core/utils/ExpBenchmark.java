c90ba142b745501e89e187f7408dc170b5d98d4c
aherbert <a.herbert@sussex.ac.uk>
2022-01-27 11:37:04 +0000

    Added exp benchmark using x values from file

--- gdsc-examples-jmh/src/main/java/uk/ac/sussex/gdsc/examples/core/utils/ExpBenchmark.java
+++ gdsc-examples-jmh/src/main/java/uk/ac/sussex/gdsc/examples/core/utils/ExpBenchmark.java
@@ -22,9 +22,18 @@
 
 package uk.ac.sussex.gdsc.examples.core.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 import java.util.function.DoubleSupplier;
 import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Stream;
 import org.apache.commons.math3.util.FastMath;
 import org.apache.commons.rng.sampling.distribution.ContinuousUniformSampler;
 import org.apache.commons.rng.simple.RandomSource;
@@ -97,8 +106,8 @@ public class ExpBenchmark {
   }
 
   /**
-   * The method to compute the exponential from a uniform value.
-   * Note: The approximate range for exp is [-746, 710]
+   * The method to compute the exponential from a uniform value. Note: The approximate range for exp
+   * is [-746, 710]
    */
   @State(Scope.Benchmark)
   public static class UniformSource extends Source {
@@ -111,7 +120,119 @@ public class ExpBenchmark {
 
     @Override
     protected DoubleSupplier createValues() {
-      return ContinuousUniformSampler.of(RandomSource.XO_RO_SHI_RO_128_PP.create(), low, high)::sample;
+      return ContinuousUniformSampler.of(RandomSource.XO_RO_SHI_RO_128_PP.create(), low,
+          high)::sample;
+    }
+  }
+
+  /**
+   * Load example exponential values from file.
+   */
+  @State(Scope.Benchmark)
+  public static class FileSource extends Source {
+    /** The values. */
+    private static DoubleSupplier values;
+
+    /** The filename. */
+    @Param({"ExpBenchmark.txt"})
+    private String filename;
+
+    @Override
+    protected DoubleSupplier createValues() {
+      DoubleSupplier v = values;
+      if (v == null) {
+        values = v = readValues();
+      }
+      return v;
+    }
+
+    private DoubleSupplier readValues() {
+      try (Stream<String> lines = lines(filename)) {
+        final double[] data = lines.mapToDouble(Double::parseDouble).toArray();
+        final int len = data.length;
+        if (len == 0) {
+          throw new IllegalStateException("No exp file data: " + filename);
+        }
+
+        // Power of 2 is efficient
+        if ((len & (len - 1)) == 0) {
+          return new DoubleSupplier() {
+            private final int mask = len - 1;
+            private int i;
+
+            @Override
+            public double getAsDouble() {
+              return data[i++ & mask];
+            }
+          };
+        }
+
+        // Non-power of 2 requires index checks
+        return new DoubleSupplier() {
+          private int i;
+
+          @Override
+          public double getAsDouble() {
+            final int j = i++;
+            if (j == data.length) {
+              // Reset next position
+              i = 1;
+              // Return first value
+              return data[0];
+            }
+            return data[j];
+          }
+        };
+      }
+    }
+
+    /**
+     * Get the lines from the resource file. This method opens resources that are closed if the
+     * stream is closed. The returned stream should be used in try-with-resources to ensure the
+     * close operation occurs.
+     *
+     * @return the stream
+     */
+    @SuppressWarnings("resource")
+    private static Stream<String> lines(String filename) {
+      // Load from file
+      final Path path = Paths.get(filename);
+      if (Files.exists(path)) {
+        try {
+          return Files.lines(path);
+        } catch (IOException e) {
+          throw new UncheckedIOException(e);
+        }
+      }
+
+      // Load from class resources.
+      // Prepend package name to workaround JMH generated packages
+      final String packageName = ExpBenchmark.class.getPackage().getName().replace('.', '/');
+      final String name = "/" + packageName + "/" + filename;
+      final InputStream is = ExpBenchmark.class.getResourceAsStream(name);
+      assert is != null : name;
+      BufferedReader br = new BufferedReader(new InputStreamReader(is));
+      try {
+        // Close the reader when the stream is closed
+        return br.lines().onClose(() -> {
+          try {
+            br.close();
+          } catch (IOException e) {
+            throw new UncheckedIOException(e);
+          }
+        });
+      } catch (Error | RuntimeException e) {
+        try {
+          br.close();
+        } catch (IOException ex) {
+          try {
+            e.addSuppressed(ex);
+          } catch (Throwable ignore) {
+            // Ignore
+          }
+        }
+        throw e;
+      }
     }
   }
 
@@ -123,6 +244,17 @@ public class ExpBenchmark {
    */
   @Benchmark
   public double uniform(UniformSource source) {
+    return source.next();
+  }
+
+  /**
+   * Compute the exponential.
+   *
+   * @param source source of the function
+   * @return the exponential
+   */
+  @Benchmark
+  public double file(FileSource source) {
     return source.next();
   }
 }
